a79f21586b9cc7e9fcc25b1bddc9d5588f4f6d15
witehound <netvid00@gmail.com>
2023-04-10 15:00:12 +0100

    Arrays with types

--- typescript/app.ts
+++ typescript/app.ts
@@ -1,5 +1,14 @@
+// simple types
 let variable = "hello"
 
 let number: number;
 
 let stringOrNumber: string | number;
+
+//Arrays with types
+
+let stringArray: string[];
+
+let numberArray: number[];
+
+let numberOrNumbrArray: number[] | string[];
