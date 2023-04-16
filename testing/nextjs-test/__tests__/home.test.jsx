391f8649e644bec728d1619c5ca0b04a2f377276
witehound <netvid00@gmail.com>
2023-04-16 20:20:12 +0100

    wrote my first jest test

--- testing/nextjs-test/__tests__/home.test.jsx
+++ testing/nextjs-test/__tests__/home.test.jsx
@@ -1,3 +1,9 @@
+import { render, screen } from "@testing-library/react";
+import Home from "../pages";
+
 describe("Home page - rendering ", () => {
-  it("Hello", () => {});
+  it("Should have home page text", () => {
+    render(<Home />);
+    screen.getAllByText("Home page");
+  });
 });
