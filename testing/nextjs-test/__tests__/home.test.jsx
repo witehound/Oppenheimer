84c9da40d9001799a5001d4e86e42c651b71d15f
witehound <netvid00@gmail.com>
2023-04-20 18:54:44 +0100

    refactored test suite

--- testing/nextjs-test/__tests__/home.test.jsx
+++ testing/nextjs-test/__tests__/home.test.jsx
@@ -1,31 +1,33 @@
 import { render, screen } from "@testing-library/react";
 import Home from "../pages";
 
-describe("Home page - rendering ", () => {
-  it("Should have home page text", () => {
-    render(<Home />);
-    expect(screen.getByText("Home page")).toBeInTheDocument();
-  });
-  it("Should have button", () => {
-    render(<Home />);
-    expect(
-      screen.getByRole("button", { name: "Click Me" })
-    ).toBeInTheDocument();
-  });
-  it("Should have input field with label Enter random text", () => {
-    render(<Home />);
-    expect(screen.getByLabelText(/Enter random text/)).toBeInTheDocument();
-  });
-  it("Should find input field by placeholder search", () => {
-    render(<Home />);
-    expect(screen.getByPlaceholderText(/search.../)).toBeInTheDocument();
-  });
-  it("Should find input field by text value", () => {
-    render(<Home />);
-    expect(screen.getByDisplayValue(/audio/)).toBeInTheDocument();
-  });
-  it("Should not find the Suprise text", () => {
-    render(<Home />);
-    expect(screen.queryByText("Suprise text")).not.toBeInTheDocument();
+describe("Home page", () => {
+  describe("Home page - rendering ", () => {
+    it("Should have home page text", () => {
+      render(<Home />);
+      expect(screen.getByText("Home page")).toBeInTheDocument();
+    });
+    it("Should have button", () => {
+      render(<Home />);
+      expect(
+        screen.getByRole("button", { name: "Click Me" })
+      ).toBeInTheDocument();
+    });
+    it("Should have input field with label Enter random text", () => {
+      render(<Home />);
+      expect(screen.getByLabelText(/Enter random text/)).toBeInTheDocument();
+    });
+    it("Should find input field by placeholder search", () => {
+      render(<Home />);
+      expect(screen.getByPlaceholderText(/search.../)).toBeInTheDocument();
+    });
+    it("Should find input field by text value", () => {
+      render(<Home />);
+      expect(screen.getByDisplayValue(/audio/)).toBeInTheDocument();
+    });
+    it("Should not find the Suprise text", () => {
+      render(<Home />);
+      expect(screen.queryByText("Suprise text")).not.toBeInTheDocument();
+    });
   });
 });
